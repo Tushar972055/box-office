b9a3915c1af55b4a2970cbc2fd0094d3c9f58cfa
Tushar <Tushar.Sharma972055@gmail.com>
2022-08-06 13:07:35 +0530

    Adding API and searchbar to the web page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,31 @@
-import React from 'react'
-import MainPageLayout from '../Components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../Components/MainPageLayout';
 
-function Home() {
-  return <MainPageLayout>This is the Home page of our website</MainPageLayout>;
-}
+const Home = () => {
+  const [input, setInput] = useState('');
+  const onInputChange = ev => {
+      setInput(ev.target.value);
+    };
+    const onSearch = () => {
+        fetch(`https://api.tvmaze.com/search/shows?q= ${input}`)
+        .then(r => r.json())
+        .then(result => {
+            console.log(result);
+        });
+    };
+    const onKeyDown = ev => {
+      if (ev.keyCode===13) {
+          onSearch();
+      }
+    }
+  return (
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
 
-export default Home
+export default Home;
