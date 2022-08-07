21820a8696b605a67a425d4829610313e2c71f88
Tushar <Tushar.Sharma972055@gmail.com>
2022-08-08 02:57:04 +0530

    Mentioned details of the actors, show, and other description

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-function Title({title, subtitle}) {
+function Title({ title, subtitle }) {
   return (
     <div>
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
     </div>
-  )
+  );
 }
 
 export default Title;
--- src/Components/actor/ActorCard.Styled.js
+++ src/Components/actor/ActorCard.Styled.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 import { SearchCard } from '../styled';
 
-export const StyledActorCard= styled(SearchCard)`
-.deathday{
-    margin:0;
-    margin-top:15px;
-    font-weight:bold;
-}
+export const StyledActorCard = styled(SearchCard)`
+  .deathday {
+    margin: 0;
+    margin-top: 15px;
+    font-weight: bold;
+  }
 `;
