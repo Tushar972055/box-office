de1131f2fb7de6423b315afbaf304d9d83b98e4d
Tushar <Tushar.Sharma972055@gmail.com>
2022-08-07 18:28:19 +0530

    created web-hooks for the read more section which shows not found every time we visit that page but now it will show the message and in the next commit we will add more information regarding the page by taking the input from the API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Show from './Pages/Show';
 import Starred from './Pages/Starred';
 
 function App() {
   return (
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>Not Found</div>
-        </Route>
-      </Switch>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+      <Route>
+        <div>Not Found</div>
+      </Route>
+    </Switch>
   );
 }
 
