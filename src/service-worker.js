d37e7b9fb6bfda26042e14e821ed6a7d28c5a1ce
Tushar <Tushar.Sharma972055@gmail.com>
2022-08-12 13:08:54 +0530

    no change

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-restricted-globals */
 
 // This service worker can be customized!
@@ -24,7 +25,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const fileExtensionRegexp = /\/[^\\/?]+\.[^\\/]+$/;
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
@@ -37,13 +38,9 @@ registerRoute(
       return false;
     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    } // Return true to signal that we want to use the handler.
-
-    return true;
+    return !(url.pathname.match(fileExtensionRegexp));
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(`${process.env.PUBLIC_URL  }/index.html`)
 );
 
 // An example runtime caching route for requests that aren't handled by the
